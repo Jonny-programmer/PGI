4705b522fe9a81766b2e139970592e247a4a9ce3
Jonny <76569574+Jonny-programmer@users.noreply.github.com>
2022-02-26 15:31:05 +0300

    Add README, files_array.js

---
+++ README.md
@@ -0,0 +1,13 @@
+FOR DEVELOPERS:
+
+1) You need to copy the following link and download files for practising.
+
+https://disk.yandex.ru/d/YRy6chw4OumnOw
+
+Download all the files directly in the /data folder, they are already added to .gitignore
+
+2) USAGE EXAMPLE OF GETTING ALL THE FILES IN DATA:
+(from PGI dir)
+
+$ npm install node
+$ node ./js/files_array.js > files.html
---
+++ js/files_array.js
@@ -0,0 +1,21 @@
+const { resolve } = require('path'),
+  { readdir } = require('fs').promises;
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(dirents.map((dirent) => {
+    const res = resolve(dir, dirent.name);
+    return dirent.isDirectory() ? getFiles(res) : res;
+  }));
+  return Array.prototype.concat(...files);
+}
+
+const directory = "./data/";
+
+getFiles(directory).then(results => {
+  const html = `<ul>` +
+  results.map(fileOrDirectory => `<li>${fileOrDirectory}</li>`).join('\n') +
+  `</ul>`;
+
+  process.stdout.write(html);
+});

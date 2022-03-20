b4dcaa6885c6a69e456fa8eac5e3f3f23bb7f46a
Jonny <76569574+Jonny-programmer@users.noreply.github.com>
2022-03-20 21:38:04 +0300

    Cleanup

--- PLOTLY/wavelett(not_working).py
+++ PLOTLY/wavelett(not_working).py
@@ -1,6 +1,8 @@
 import plotly.express as px
 import numpy as np
 from h5py import File
+from scipy import signal
+
 
 file = File('/Users/eremin/Documents/GitHub/PGI/static/mat/2022-02-04-d3.mat')
 
@@ -11,8 +13,10 @@ unix_time = np.append(unix_time, last)
 unix_time = [np.linspace(unix_time[i], unix_time[i + 1], 128) for i in range(len(unix_time) - 1)]
 UNIX_TIME = np.concatenate(unix_time)
 
-data = np.concatenate(file['cwt_global'])
-
+q = 1170
+data = file['cwt_global']
+data = np.concatenate(data)
+data = signal.decimate(data, q=q, ftype='fir', n=8)
 wave = px.imshow(data, x=UNIX_TIME, contrast_rescaling='minmax',
                   title="Wavelett", aspect='auto')
 wave.show()
--- app.py
+++ app.py
@@ -1,39 +1,16 @@
-from bokeh.plotting import figure
-from h5py import File
-import numpy as np
-from bokeh.embed import components
-import flask_bootstrap
 from flask import Flask, render_template, url_for, request, redirect
 from flask_login import LoginManager
 
 # Создание основного приложения
 app = Flask(__name__)
-bootstrap = flask_bootstrap.Bootstrap(app)
 login = LoginManager(app)
 login.init_app(app)
 login.login_view = 'login'
 
 
-file = File('static/mat/2022-02-04-d3.mat')
-
-light_curve = file['lightcurvesum_global']
-unix_time = file['unixtime_global']
-last = np.add(unix_time[-1], 5)
-unix_time = np.append(unix_time, last)
-unix_time = [np.linspace(unix_time[i], unix_time[i + 1], 128) for i in range(len(unix_time) - 1)]
-
-x = np.concatenate(unix_time)
-y = np.concatenate(light_curve)
-
-
-plot = figure(title='Light curve', x_axis_label='Time, 10^5', y_axis_label='Intensity', width=1000)
-plot.line(x, y, legend_label='', line_width=2)
-script, div = components(plot)
-
-
 @app.route('/')
 def index():
-    return render_template('index.html', script=script, div=div)
+    return render_template('templates/index.html')
 
 
 # Route for handling the login page logic

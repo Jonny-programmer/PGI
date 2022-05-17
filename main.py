6e28846d4c94eae4f5a2928a286c4a9ba618957b
Jonny-programmer <76569574+Jonny-programmer@users.noreply.github.com>
2022-05-17 20:09:39 +0300

    Fixed 764 line

--- main.py
+++ main.py
@@ -760,8 +760,8 @@ def all_data_files():
 
 
 if __name__ == "__main__":
-    # app.run('0.0.0.0', port=5000, debug=True)
-    app.run('127.0.0.1', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
+    # app.run('127.0.0.1', port=5000, debug=True)
     # serve(app, host='0.0.0.0', port=5000)
     if server:
         server.quit()
